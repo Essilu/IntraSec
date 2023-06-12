79eb431472de33e8378d95cb3cc50a145889d5e4
ulysse <juget.ulysse@gmail.com>
2023-06-12 17:30:55 +0200

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
-import { Button, MantineProvider } from '@mantine/core';
+//import "./pages/styles/App.css";
+import { Button, MantineProvider } from "@mantine/core";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Button>SecureCorp</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import { TextInput, Checkbox, Button, Group, Box, Image, BackgroundImage } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import axios from "axios";
+
+import "./styles/login.css";
+
+export default function Login() {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
+
+  const submit = (formValues) => {
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/auth/login`, formValues)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+    };
+
+  return (
+    <div className="parent">
+    <Box maw={500} mx="auto" className="signin">
+    <Image withPlaceholder src="/src/assets/logo.png"/>
+      <form className="text" onSubmit={form.onSubmit(submit)}>
+        <TextInput
+          withAsterisk
+          style={{ fontSize: '16px' }}
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps("email")}
+        />
+
+        <TextInput
+          withAsterisk
+          label="Password"
+          placeholder="my_secure_password123"
+          {...form.getInputProps("password")}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Sign in</Button>
+        </Group>
+      </form>
+    </Box>
+    </div>
+  );
+}
