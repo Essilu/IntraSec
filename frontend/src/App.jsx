d83bbd5483ec04fb4ae1b04678297da09a3ec5bb
Esteban_Duffourg <esteban.duffourg@efrei.net>
2023-06-13 12:29:44 +0200

    Added comptability page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Navbar from "./components/navbar";
+import Comptability from "./pages/Comptability";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navbar><Home /></Navbar>}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/comptability" element={<Navbar><Comptability /></Navbar>}></Route>
         </Routes>
       </BrowserRouter>
     </MantineProvider>
