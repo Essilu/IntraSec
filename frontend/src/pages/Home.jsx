6a1f8f895872ec93479d1507ed91eb63f335186d
ulysse <juget.ulysse@gmail.com>
2023-06-13 12:24:16 +0200

    homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import "../styles/home.css";
+
 export default function Home() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const user = 'Adrien Tabouret'; // Replace 'John Doe' with the current user's name
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Home">
+      <h1 className="time">{currentTime.toLocaleTimeString()}</h1>
+      <h2 className="welcome">Welcome Back, {user}</h2>
     </div>
   );
 }
