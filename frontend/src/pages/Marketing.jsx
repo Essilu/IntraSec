cde068c2b430d03ba1bbc538ef300e79b1bbef07
colilany <audrey.calmon@efrei.net>
2023-06-26 11:30:25 +0200

    Added article pages and buttons in marketing

--- frontend/src/pages/Marketing.jsx
+++ frontend/src/pages/Marketing.jsx
@@ -1,222 +1,244 @@
-import {
-  createStyles,
-  Title,
-  Text,
-  Button,
-  Container,
-  rem,
-} from "@mantine/core";
-import { Carousel } from "@mantine/carousel";
-import { useMediaQuery } from "@mantine/hooks";
-import { Paper, useMantineTheme } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { createStyles, Title, Text, Button, Modal, Container, rem, TextInput, Textarea} from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+import { useMediaQuery } from '@mantine/hooks';
+import { Paper, useMantineTheme } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import '../styles/Marketing.css';
 
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    position: "relative",
-    paddingTop: rem(120),
-    paddingBottom: rem(80),
 
-    [theme.fn.smallerThan("sm")]: {
-      paddingTop: rem(80),
-      paddingBottom: rem(60),
-    },
-  },
-
-  inner: {
-    position: "relative",
-    zIndex: 1,
-  },
-
-  title: {
-    textAlign: "center",
-    fontWeight: 800,
-    fontSize: rem(40),
-    letterSpacing: -1,
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    marginBottom: theme.spacing.xs,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-
-    [theme.fn.smallerThan("xs")]: {
-      fontSize: rem(28),
-      textAlign: "left",
-    },
-  },
-
-  highlight: {
-    color:
-      theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6],
-  },
-
-  description: {
-    textAlign: "center",
-
-    [theme.fn.smallerThan("xs")]: {
-      textAlign: "left",
-      fontSize: theme.fontSizes.md,
-    },
-  },
-
-  controls: {
-    marginTop: theme.spacing.lg,
-    display: "flex",
-    justifyContent: "center",
-
-    [theme.fn.smallerThan("xs")]: {
-      flexDirection: "column",
-    },
-  },
-
-  control: {
-    "&:not(:first-of-type)": {
-      marginLeft: theme.spacing.md,
-    },
-
-    [theme.fn.smallerThan("xs")]: {
-      height: rem(42),
-      fontSize: theme.fontSizes.md,
-
-      "&:not(:first-of-type)": {
-        marginTop: theme.spacing.md,
-        marginLeft: 0,
-      },
-    },
-  },
-  card: {
-    height: rem(440),
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-
-  titleCard: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontWeight: 900,
-    color: theme.white,
-    lineHeight: 1.2,
-    fontSize: rem(32),
-    marginTop: theme.spacing.xs,
-  },
-
-  category: {
-    color: theme.white,
-    opacity: 0.7,
-    fontWeight: 700,
-    textTransform: "uppercase",
-  },
-}));
-
-const data = [
+const initialCarouselData = [
   {
     image:
-      "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    titleCard: "Audit et Securité Offensive",
-    category: "Sécurité",
+      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    titleCard: 'Audit et Sécurité Offensive',
+    category: 'Sécurité',
   },
   {
     image:
-      "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    titleCard: "CyberSOC & Menace Intelligente",
-    category: "Prévention",
+      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    titleCard: 'CyberSOC & Menace Intelligente',
+    category: 'Prévention',
   },
   {
     image:
-      "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    titleCard: "Centre de Gestion des Vulnérabilités",
-    category: "Prévention",
+      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    titleCard: 'Centre de Gestion des Vulnérabilités',
+    category: 'Prévention',
   },
   {
     image:
-      "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    titleCard: "Sécurité des réseaux et des infrastructures",
-    category: "Surêté",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    titleCard: "Formation en Cybersecurité",
-    category: "Formation",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-    titleCard: "Services de test d'intrusion et de sécurité des systèmes",
-    category: "Surêté",
+      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    titleCard: 'Sécurité des réseaux et des infrastructures',
+    category: 'Sûreté',
   },
 ];
 
-function Card({ image, titleCard, category }) {
-  const { classes } = useStyles();
+function Card({ image, titleCard, category, onEdit, onDelete }) {
+  const handleEdit = () => {
+    onEdit({ image, titleCard, category });
+  };
+  
+  const handleDeleteCard = () => {
+  onDelete();
+};
+
 
   return (
     <Paper
       shadow="md"
-      p="xl"
+      padding="xl"
       radius="md"
-      sx={{ backgroundImage: `url(${image})` }}
-      className={classes.card}
+      style={{ backgroundImage: `url(${image})` }}
+      className="marketing-card"
     >
       <div>
-        <Text className={classes.category} size="xs">
+        <Text className="marketing-category" size="xs">
           {category}
         </Text>
-        <Title order={3} className={classes.titleCard}>
+        <Title order={3} className="marketing-titleCard">
           {titleCard}
         </Title>
       </div>
-      <Link to="/Article">
-        <Button variant="white" color="dark">
+      <div className="marketing-controlButtonsCard">
+        <Link to="/Article" className="marketing-seeArticle">
+        <Button variant="white" color="dark" className="marketing-viewMoreButton">
           Voir plus
+          </Button>
+          </Link>
+        <Button variant="white" color="blue" onClick={handleEdit} className="marketing-editButton">
+          Modifier
         </Button>
-      </Link>
+        
+        <Button variant="white" color="red" onClick={handleDeleteCard} className="marketing-deleteButton">
+          Supprimer
+          </Button>
+        
+      </div>
     </Paper>
   );
 }
 
-export default function Marketing() {
-  const { classes } = useStyles();
+function Marketing() {
   const theme = useMantineTheme();
-  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-  const slides = data.map((item) => (
-    <Carousel.Slide key={item.title}>
-      <Card {...item} />
-    </Carousel.Slide>
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
+  const [carouselData, setCarouselData] = useState(initialCarouselData);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [editedCard, setEditedCard] = useState(null);
+
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [newCardData, setNewCardData] = useState({
+    image: '',
+    titleCard: '',
+    category: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleAddCard = () => {
+    setShowAddModal(true);
+  };
+
+  const handleAddModalClose = () => {
+    setShowAddModal(false);
+    setNewCardData({ image: '', titleCard: '', category: '' });
+    setErrorMessage('');
+  };
+
+  const handleNewCardInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewCardData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSaveNewCard = () => {
+    if (!newCardData.image || !newCardData.titleCard || !newCardData.category) {
+      setErrorMessage('Veuillez remplir tous les champs');
+      return;
+    }
+
+    const newCard = { ...newCardData };
+    setCarouselData((prevData) => [...prevData, newCard]);
+
+    handleAddModalClose();
+  };
+
+
+  const handleEditCard = (card) => {
+    setEditedCard(card);
+    setEditModalOpen(true);
+  };
+
+  const handleDeleteCard = (index) => {
+    const updatedCarouselData = [...carouselData];
+    updatedCarouselData.splice(index, 1);
+    setCarouselData(updatedCarouselData);
+  };
+
+
+  const handleEditModalClose = () => {
+    setEditModalOpen(false);
+    setEditedCard(null);
+  };
+
+  const handleSaveChanges = (newCard) => {
+    const updatedCarouselData = carouselData.map((item) => {
+      if (item === editedCard) {
+        return { ...newCard };
+      }
+      return item;
+    });
+    setCarouselData(updatedCarouselData);
+    setEditModalOpen(false);
+    setEditedCard(null);
+  };
+
+    const slides = carouselData.map((slide, index) => (
+  <Carousel.Slide key={slide.titleCard}>
+    <Card {...slide} onEdit={() => handleEditCard(slide)} onDelete={() => handleDeleteCard(index)} />
+  </Carousel.Slide>
   ));
+
+ 
 
   return (
     <>
-      <Container className={classes.wrapper} size={1400}>
-        <div className={classes.inner}>
-          <Title className={classes.title}>
-            Découvrez le{" "}
-            <Text component="span" className={classes.highlight} inherit>
-              future de la cybersecurité
-            </Text>{" "}
-            avec SecureCorp
-          </Title>
+      <Container className="marketing-wrapper" size={1400}>
+        <div className="marketing-inner">
+        <Title className="marketing-title">
+          Découvrez le {' '}
+          <Text component="span" className="marketing-highlight" inherit>
+            future de la cybersecurité
+          </Text>{' '}
+          avec SecureCorp
+        </Title>
 
-          <Container p={0} size={600}>
-            <Text size="lg" color="dimmed" className={classes.description}>
-              Découvrez nos solutions innovantes en matière de sécurité
-              informatique. Nous vous proposons des solutions adaptées à vos
-              besoins et à votre budget.
-            </Text>
-          </Container>
-        </div>
+        <Container p={0} size={600}>
+          <Text size="lg" color="dimmed" className="marketing-description">
+            Découvrez nos solutions innovantes en matière de sécurité informatique.
+            Nous vous proposons des solutions adaptées à vos besoins et à votre budget.
+          </Text>
+        </Container>
+
+      </div>
       </Container>
+
+      <h1> Nos solutions </h1>
 
       <Carousel
         slideSize="50%"
-        breakpoints={[{ maxWidth: "sm", slideSize: "100%", slideGap: rem(2) }]}
+        breakpoints={[{ maxWidth: 'sm', slideSize: '100%', slideGap: rem(2) }]}
         slideGap="xl"
         align="start"
         slidesToScroll={mobile ? 1 : 2}
       >
         {slides}
       </Carousel>
+
+      <div id="marketing-buttonChange">
+        <Button variant="outline" color="green" onClick={handleAddCard}>
+          Ajouter
+        </Button>
+      </div>
+
+      <Modal className='marketing-modalAddCard'
+        opened={showAddModal}
+        onClose={handleAddModalClose}
+        title="Ajouter une carte"
+      >
+        <TextInput
+          label="Image"
+          placeholder="URL de l'image"
+          value={newCardData.image}
+          onChange={handleNewCardInputChange}
+          name="image"
+          required
+        />
+        <TextInput
+          label="Titre"
+          placeholder="Titre de la carte"
+          value={newCardData.titleCard}
+          onChange={handleNewCardInputChange}
+          name="titleCard"
+          required
+        />
+        <TextInput
+          label="Catégorie"
+          placeholder="Catégorie de la carte"
+          value={newCardData.category}
+          onChange={handleNewCardInputChange}
+          name="category"
+          required
+        />
+
+        {errorMessage && <div>{errorMessage}</div>}
+
+        <Button onClick={handleSaveNewCard}>Ajouter</Button>
+      </Modal>
+      
     </>
   );
 }
+
+export default Marketing;
