4205d06a79212c5c79776a6cc8e658b8a04f2302
colilany <audrey.calmon@efrei.net>
2023-06-27 01:06:24 +0200

    Comments added to marketing and article pages

--- frontend/src/pages/Article.jsx
+++ frontend/src/pages/Article.jsx
@@ -1,47 +1,141 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Container, Title, Text } from '@mantine/core';
-import '../styles/Article.css';
+import { Container, Title, Text, Button, Modal, Textarea } from "@mantine/core";
+import { useState, useEffect } from "react";
+import "../styles/Article.css";
 
 function Article() {
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [editedContent, setEditedContent] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    // Simulate fetching the article content from the server
+    setTimeout(() => {
+      setEditedContent({
+        title: "Audit et Sécurité Offensive",
+        body: `Chez SecureCorp, nous comprenons l'importance critique d'une sécurité informatique solide pour protéger vos précieuses données et assurer la continuité de votre activité. C'est pourquoi nous vous offrons notre expertise en matière d'Audit et Sécurité Offensive pour évaluer et renforcer la résistance de votre infrastructure informatique.
+
+        Notre équipe de consultants expérimentés en sécurité informatique est dévouée à identifier les vulnérabilités potentielles et les points faibles de votre système. Grâce à notre approche méthodique et rigoureuse, nous effectuons des tests approfondis, y compris des tentatives d'intrusion contrôlées, pour détecter les éventuelles failles de sécurité et les exploiter avant que les acteurs malveillants ne le fassent.
+
+        En collaborant étroitement avec votre entreprise, nous élaborons des stratégies personnalisées et des recommandations concrètes pour renforcer votre posture de sécurité. Que ce soit en identifiant les lacunes dans votre architecture réseau, en évaluant vos politiques de sécurité ou en effectuant des audits de conformité, nous sommes là pour vous aider à anticiper les menaces et à mettre en place des mesures préventives adaptées.
+
+        Notre service d'Audit et Sécurité Offensive vise à vous offrir une tranquillité d'esprit totale en sachant que votre système informatique est protégé contre les attaques potentielles. En renforçant vos défenses, nous minimisons les risques de perturbation des opérations, de vol de données confidentielles ou de réputation endommagée.
+
+        Choisir SecureCorp, c'est opter pour une approche proactive en matière de sécurité informatique. Avec notre expertise avancée et notre souci du détail, nous vous garantissons une évaluation complète de vos vulnérabilités, des recommandations adaptées à votre environnement spécifique et une assistance continue pour maintenir votre système à l'abri des menaces en constante évolution.
+
+        Protégez votre entreprise, votre réputation et la confiance de vos clients en faisant confiance à SecureCorp pour votre Audit et Sécurité Offensive. Contactez-nous dès maintenant pour une consultation personnalisée et prenez les devants dans le domaine de la sécurité informatique. Votre tranquillité d'esprit n'a pas de prix !`,
+      });
+    }, 500);
+  }, []);
+
+  // Event handlers for opening the edit and info modals
+  const handleEditModalClose = () => {
+    setEditModalOpen(false);
+    setErrorMessage("");
+  };
+
+  // Event handler for saving the changes
+  const handleSaveChanges = (newContent) => {
+    if (validateContent(newContent)) {
+      // Effectuer les actions de sauvegarde ici
+      setEditModalOpen(false);
+      setErrorMessage("");
+      console.log(newContent);
+    } else {
+      setErrorMessage("Veuillez remplir tous les champs");
+    }
+  };
+
+  // Event handler for opening the edit modal
+  const handleEditContent = () => {
+    setEditModalOpen(true);
+  };
+
+  // Validate the content of the article
+  const validateContent = (content) => {
+    return content?.title && content?.body;
+  };
+
+  // Render the component
   return (
     <>
-      <div className= "article-root">
+      <div className="article-root">
         <Container size="lg">
-          <div className= "article-inner">
-            <div className= "article-content">
-              {/* Title component */}
-              <Title className= "article-title">
-                <Text
-                  component="span"
-                  inherit
-                  variant="gradient"
-                  gradient={{ from: 'pink', to: 'yellow' }}
-                >
-                  Audit et Sécurité Offensive
-                </Text>{' '}
-                Maximisez la robustesse de votre système informatique !
-              </Title>
+          <div className="article-inner">
+            <div className="article-content">
+              <div className="article-title-wrapper">
+                <div className="article-title-background" />
+                <Title className="article-title">
+                  {/* Display the article title here */}
+                  <Text
+                    component="span"
+                    inherit
+                    variant="gradient"
+                    gradient={{ from: "pink", to: "yellow" }}
+                  >
+                    {editedContent?.title || "Titre de l'article"}
+                  </Text>{" "}
+                  Maximisez la robustesse de votre système informatique !
+                </Title>
+                {/* Display modification button for the article */}
+                <div className="article-button">
+                  <Button
+                    variant="white"
+                    color="blue"
+                    onClick={handleEditContent}
+                  >
+                    Modifier
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </Container>
       </div>
+
+      {/* Display the article description for the changes */}
       <Container>
-    
-        <Text className= "article-description" mt={30}>
-          Chez SecureCorp, nous comprenons l'importance critique d'une sécurité informatique solide pour protéger vos précieuses données et assurer la continuité de votre activité. C'est pourquoi nous vous offrons notre expertise en matière d'Audit et Sécurité Offensive pour évaluer et renforcer la résistance de votre infrastructure informatique.
-
-          Notre équipe de consultants expérimentés en sécurité informatique est dévouée à identifier les vulnérabilités potentielles et les points faibles de votre système. Grâce à notre approche méthodique et rigoureuse, nous effectuons des tests approfondis, y compris des tentatives d'intrusion contrôlées, pour détecter les éventuelles failles de sécurité et les exploiter avant que les acteurs malveillants ne le fassent.
-
-          En collaborant étroitement avec votre entreprise, nous élaborons des stratégies personnalisées et des recommandations concrètes pour renforcer votre posture de sécurité. Que ce soit en identifiant les lacunes dans votre architecture réseau, en évaluant vos politiques de sécurité ou en effectuant des audits de conformité, nous sommes là pour vous aider à anticiper les menaces et à mettre en place des mesures préventives adaptées.
-
-          Notre service d'Audit et Sécurité Offensive vise à vous offrir une tranquillité d'esprit totale en sachant que votre système informatique est protégé contre les attaques potentielles. En renforçant vos défenses, nous minimisons les risques de perturbation des opérations, de vol de données confidentielles ou de réputation endommagée.
-
-          Choisir SecureCorp, c'est opter pour une approche proactive en matière de sécurité informatique. Avec notre expertise avancée et notre souci du détail, nous vous garantissons une évaluation complète de vos vulnérabilités, des recommandations adaptées à votre environnement spécifique et une assistance continue pour maintenir votre système à l'abri des menaces en constante évolution.
-
-          Protégez votre entreprise, votre réputation et la confiance de vos clients en faisant confiance à SecureCorp pour votre Audit et Sécurité Offensive. Contactez-nous dès maintenant pour une consultation personnalisée et prenez les devants dans le domaine de la sécurité informatique. Votre tranquillité d'esprit n'a pas de prix !
-        </Text>
+        <div className="article-description">
+          <Text>{editedContent?.body}</Text>
+        </div>
       </Container>
+
+      {/* Display the edit modal */}
+      <Modal
+        opened={editModalOpen}
+        onClose={handleEditModalClose}
+        title="Modifier le contenu de l'article"
+      >
+        <Textarea
+          label="Titre"
+          placeholder="Titre de l'article"
+          value={editedContent?.title || ""}
+          onChange={(event) =>
+            setEditedContent({ ...editedContent, title: event.target.value })
+          }
+          required
+        />
+
+        <Textarea
+          label="Corps de l'article"
+          placeholder="Contenu de l'article"
+          value={editedContent?.body || ""}
+          onChange={(event) =>
+            setEditedContent({ ...editedContent, body: event.target.value })
+          }
+          required
+          rows={10}
+        />
+
+        {/* Display the error message if there is one */}
+        {errorMessage && <div>{errorMessage}</div>}
+
+        <Button onClick={() => handleSaveChanges(editedContent)}>
+          Enregistrer
+        </Button>
+      </Modal>
     </>
   );
 }
+
 export default Article;
--- frontend/src/pages/Marketing.jsx
+++ frontend/src/pages/Marketing.jsx
@@ -1,43 +1,55 @@
-import { useState } from 'react';
-import { Title, Text, Button, Modal, Container, rem, TextInput } from '@mantine/core';
-import { Carousel } from '@mantine/carousel';
-import { useMediaQuery } from '@mantine/hooks';
-import { Paper, useMantineTheme } from '@mantine/core';
-import { Link } from 'react-router-dom';
-import '../styles/Marketing.css';
+import { useState } from "react";
+import {
+  Title,
+  Text,
+  Button,
+  Modal,
+  Container,
+  rem,
+  TextInput,
+} from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import { useMediaQuery } from "@mantine/hooks";
+import { Paper, useMantineTheme } from "@mantine/core";
+import { Link } from "react-router-dom";
+import "../styles/Marketing.css";
 
+// Define the initial carousel data
 const initialCarouselData = [
   {
     image:
-      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    titleCard: 'Audit et Sécurité Offensive',
-    category: 'Sécurité',
+      "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    titleCard: "Audit et Sécurité Offensive",
+    category: "Sécurité",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    titleCard: 'CyberSOC & Menace Intelligente',
-    category: 'Prévention',
+      "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    titleCard: "CyberSOC & Menace Intelligente",
+    category: "Prévention",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    titleCard: 'Centre de Gestion des Vulnérabilités',
-    category: 'Prévention',
+      "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    titleCard: "Centre de Gestion des Vulnérabilités",
+    category: "Prévention",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    titleCard: 'Sécurité des réseaux et des infrastructures',
-    category: 'Sûreté',
+      "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    titleCard: "Sécurité des réseaux et des infrastructures",
+    category: "Sûreté",
   },
 ];
 
+// Define the Card component
 function Card({ image, titleCard, category, onEdit, onDelete }) {
+  // Render the component
   const handleEdit = () => {
     onEdit({ image, titleCard, category });
   };
 
+  // Event handler for deleting the card
   const handleDeleteCard = () => {
     onDelete();
   };
@@ -59,16 +71,32 @@ function Card({ image, titleCard, category, onEdit, onDelete }) {
         </Title>
       </div>
       <div className="marketing-controlButtonsCard">
+        {/* Update show more button to navigate to the corresponding page*/}
+        {/* <Link to={`/CardPage/${titleCard}`} className="marketing-seeArticle"> */}
         <Link to="/Article" className="marketing-seeArticle">
-          <Button variant="white" color="dark" className="marketing-viewMoreButton">
+          <Button
+            variant="white"
+            color="dark"
+            className="marketing-viewMoreButton"
+          >
             Voir plus
           </Button>
         </Link>
-        <Button variant="white" color="blue" onClick={handleEdit} className="marketing-editButton">
+        <Button
+          variant="white"
+          color="blue"
+          onClick={handleEdit} // Update the event handler
+          className="marketing-editButton"
+        >
           Modifier
         </Button>
 
-        <Button variant="white" color="red" onClick={handleDeleteCard} className="marketing-deleteButton">
+        <Button
+          variant="white"
+          color="red"
+          onClick={handleDeleteCard} // Update the event handler
+          className="marketing-deleteButton"
+        >
           Supprimer
         </Button>
       </div>
@@ -76,31 +104,36 @@ function Card({ image, titleCard, category, onEdit, onDelete }) {
   );
 }
 
+// Define the Marketing component
 function Marketing() {
+  // Define state variables using the useState hook
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
   const [carouselData, setCarouselData] = useState(initialCarouselData);
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [editedCard, setEditedCard] = useState(null);
-
+  // Define state variables for the add modal
   const [showAddModal, setShowAddModal] = useState(false);
   const [newCardData, setNewCardData] = useState({
-    image: '',
-    titleCard: '',
-    category: '',
+    image: "",
+    titleCard: "",
+    category: "",
   });
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
+  // Event handler for opening the add modal
   const handleAddCard = () => {
     setShowAddModal(true);
   };
 
+  // Event handler for closing the add modal
   const handleAddModalClose = () => {
     setShowAddModal(false);
-    setNewCardData({ image: '', titleCard: '', category: '' });
-    setErrorMessage('');
+    setNewCardData({ image: "", titleCard: "", category: "" });
+    setErrorMessage("");
   };
 
+  // Event handler for updating the new card data
   const handleNewCardInputChange = (event) => {
     const { name, value } = event.target;
     setNewCardData((prevData) => ({
@@ -109,9 +142,10 @@ function Marketing() {
     }));
   };
 
+  // Event handler for saving the new card
   const handleSaveNewCard = () => {
     if (!newCardData.image || !newCardData.titleCard || !newCardData.category) {
-      setErrorMessage('Veuillez remplir tous les champs');
+      setErrorMessage("Veuillez remplir tous les champs");
       return;
     }
 
@@ -121,38 +155,44 @@ function Marketing() {
     handleAddModalClose();
   };
 
+  // Event handler for deleting a card
   const handleDeleteCard = (index) => {
     const updatedCarouselData = [...carouselData];
     updatedCarouselData.splice(index, 1);
     setCarouselData(updatedCarouselData);
   };
 
+  // Event handler for closing the edit modal
   const handleEditModalClose = () => {
     setEditModalOpen(false);
     setEditedCard(null);
   };
 
-const handleSaveChanges = (newCard) => {
-  const updatedCarouselData = [...carouselData];
-  updatedCarouselData[editedCard.index] = newCard;
-  setCarouselData(updatedCarouselData);
-  setEditModalOpen(false);
-  setEditedCard(null);
-  console.log(newCard);
-};
+  // Event handler for saving the changes
+  const handleSaveChanges = (newCard) => {
+    const updatedCarouselData = [...carouselData];
+    updatedCarouselData[editedCard.index] = newCard;
+    setCarouselData(updatedCarouselData);
+    setEditModalOpen(false);
+    setEditedCard(null);
+    console.log(newCard);
+  };
 
+  // Event handler for opening the edit modal
+  const handleEditCard = (card) => {
+    const index = carouselData.findIndex((item) => item === card);
+    setEditedCard({ ...card, index });
+    setEditModalOpen(true);
+  };
 
-
- const handleEditCard = (card) => {
-  const index = carouselData.findIndex((item) => item === card);
-  setEditedCard({ ...card, index });
-  setEditModalOpen(true);
-};
-
-
+  // Render the component with the carrousel
   const slides = carouselData.map((slide, index) => (
     <Carousel.Slide key={slide.titleCard}>
-      <Card {...slide} onEdit={() => handleEditCard(slide)} onDelete={() => handleDeleteCard(index)} />
+      <Card
+        {...slide}
+        onEdit={() => handleEditCard(slide)}
+        onDelete={() => handleDeleteCard(index)}
+      />
     </Carousel.Slide>
   ));
 
@@ -161,16 +201,18 @@ const handleSaveChanges = (newCard) => {
       <Container className="marketing-wrapper" size={1400}>
         <div className="marketing-inner">
           <Title className="marketing-title">
-            Découvrez le <Text component="span" className="marketing-highlight" inherit>
+            Découvrez le{" "}
+            <Text component="span" className="marketing-highlight" inherit>
               future de la cybersecurité
-            </Text>{' '}
+            </Text>{" "}
             avec SecureCorp
           </Title>
 
           <Container p={0} size={600}>
             <Text size="lg" color="dimmed" className="marketing-description">
-              Découvrez nos solutions innovantes en matière de sécurité informatique. Nous vous proposons des solutions
-              adaptées à vos besoins et à votre budget.
+              Découvrez nos solutions innovantes en matière de sécurité
+              informatique. Nous vous proposons des solutions adaptées à vos
+              besoins et à votre budget.
             </Text>
           </Container>
         </div>
@@ -178,9 +220,9 @@ const handleSaveChanges = (newCard) => {
 
       <h1> Nos solutions </h1>
 
-      <Carousel
+      <Carousel // Update the Carousel component
         slideSize="50%"
-        breakpoints={[{ maxWidth: 'sm', slideSize: '100%', slideGap: rem(2) }]}
+        breakpoints={[{ maxWidth: "sm", slideSize: "100%", slideGap: rem(2) }]}
         slideGap="xl"
         align="start"
         slidesToScroll={mobile ? 1 : 2}
@@ -194,46 +236,52 @@ const handleSaveChanges = (newCard) => {
         </Button>
       </div>
 
-      <Modal
+      <Modal // Update the Modal component to modify a card of the carrousel
         opened={editModalOpen}
         onClose={handleEditModalClose}
         title="Modifier une carte"
       >
-        <TextInput
+        <TextInput // Update the image
           label="Image"
           placeholder="URL de l'image"
-          value={editedCard?.image || ''}
-          onChange={(event) => setEditedCard({ ...editedCard, image: event.target.value })}
+          value={editedCard?.image || ""}
+          onChange={(event) =>
+            setEditedCard({ ...editedCard, image: event.target.value })
+          }
           required
         />
-        <TextInput
+        <TextInput // Update the title
           label="Titre"
           placeholder="Titre de la carte"
-          value={editedCard?.titleCard || ''}
-          onChange={(event) => setEditedCard({ ...editedCard, titleCard: event.target.value })}
+          value={editedCard?.titleCard || ""}
+          onChange={(event) =>
+            setEditedCard({ ...editedCard, titleCard: event.target.value })
+          }
           required
         />
-        <TextInput
+        <TextInput // Update the category
           label="Catégorie"
           placeholder="Catégorie de la carte"
-          value={editedCard?.category || ''}
-          onChange={(event) => setEditedCard({ ...editedCard, category: event.target.value })}
+          value={editedCard?.category || ""}
+          onChange={(event) =>
+            setEditedCard({ ...editedCard, category: event.target.value })
+          }
           required
         />
-
-        {errorMessage && <div>{errorMessage}</div>}
-
-
-        <Button onClick={() => handleSaveChanges(editedCard)}>Enregistrer</Button>
+        {errorMessage && <div>{errorMessage}</div>}{" "}
+        {/* Display the error message in case */}
+        <Button onClick={() => handleSaveChanges(editedCard)}>
+          Enregistrer
+        </Button>
       </Modal>
 
-      <Modal
+      <Modal // Update the Modal component to add a card to the carrousel
         className="marketing-modalAddCard"
         opened={showAddModal}
         onClose={handleAddModalClose}
         title="Ajouter une carte"
       >
-        <TextInput
+        <TextInput // Update the image
           label="Image"
           placeholder="URL de l'image"
           value={newCardData.image}
@@ -241,7 +289,7 @@ const handleSaveChanges = (newCard) => {
           name="image"
           required
         />
-        <TextInput
+        <TextInput // Update the title
           label="Titre"
           placeholder="Titre de la carte"
           value={newCardData.titleCard}
@@ -249,7 +297,7 @@ const handleSaveChanges = (newCard) => {
           name="titleCard"
           required
         />
-        <TextInput
+        <TextInput // Update the category
           label="Catégorie"
           placeholder="Catégorie de la carte"
           value={newCardData.category}
@@ -257,9 +305,8 @@ const handleSaveChanges = (newCard) => {
           name="category"
           required
         />
-
-        {errorMessage && <div>{errorMessage}</div>}
-
+        {errorMessage && <div>{errorMessage}</div>}{" "}
+        {/* Display the error message in case */}
         <Button onClick={handleSaveNewCard}>Ajouter</Button>
       </Modal>
     </>
