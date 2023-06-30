2d5b3522739f3d4a75fd28dd324d1ddfc1d50bf6
Elliot Maisl <elliot.maisl@gmail.com>
2023-06-30 19:15:12 +0200

    Add @mantine/modals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from '@mantine/modals';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./stores/auth";
 import Home from "./pages/Home";
@@ -16,6 +17,7 @@ import SchoolList from "./pages/SchoolList";
 import Article from "./pages/Article";
 import Protected from "./components/Protected";
 import { useMemo } from 'react';
+import Roles from './pages/Roles';
 
 function App() {
   const [isLogged, fetchMe] = useAuthStore(state => [state.isLogged, state.fetchMe]);
@@ -35,26 +37,29 @@ function App() {
 
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={makePage(<Home />)} />
-          <Route path="/login" element={
-            isLogged()
-              ? <Navigate to="/" replace />
-              : <Login />
-          } />
-          <Route path="/comptability" element={makePage(<Comptability />)} />
-          <Route path="/TransactionsHistory" element={makePage(<TransactionsHistory />)} />
-          <Route path="/EmployeesAdmin" element={makePage(<EmployeesAdmin />)} />
-          <Route path="/EmployeesList" element={makePage(<EmployeesList />)} />
-          <Route path="/NewTicket" element={makePage(<NewTicket />)} />
-          <Route path="/Tickets" element={makePage(<Tickets />)} />
-          <Route path="/Marketing" element={makePage(<Marketing />)} />
-          <Route path="/CompanyList" element={makePage(<CompanyList />)} />
-          <Route path="/Article/:id" element={makePage(<Article/>)} />
-          <Route path="/SchoolList" element={makePage(<SchoolList />)} />
-        </Routes>
-      </BrowserRouter>
+      <ModalsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={makePage(<Home />)} />
+            <Route path="/login" element={
+              isLogged()
+                ? <Navigate to="/" replace />
+                : <Login />
+            } />
+            <Route path="/comptability" element={makePage(<Comptability />)} />
+            <Route path="/TransactionsHistory" element={makePage(<TransactionsHistory />)} />
+            <Route path="/EmployeesAdmin" element={makePage(<EmployeesAdmin />)} />
+            <Route path="/EmployeesList" element={makePage(<EmployeesList />)} />
+            <Route path="/NewTicket" element={makePage(<NewTicket />)} />
+            <Route path="/Tickets" element={makePage(<Tickets />)} />
+            <Route path="/Marketing" element={makePage(<Marketing />)} />
+            <Route path="/CompanyList" element={makePage(<CompanyList />)} />
+            <Route path="/Article/:id" element={makePage(<Article/>)} />
+            <Route path="/SchoolList" element={makePage(<SchoolList />)} />
+            <Route path="/Roles" element={makePage(<Roles />)} />
+          </Routes>
+        </BrowserRouter>
+      </ModalsProvider>
     </MantineProvider>
   );
 }
